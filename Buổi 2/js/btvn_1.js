3171020f68349cb39358ea6ca5d4f7420445f439
MaiNgocChau9 <chauaurora9@gmail.com>
2024-11-01 22:12:37 +0700

    Thêm file HTML với script JavaScript minh họa thao tác chuỗi và kiểu dữ liệu

---
+++ Buổi 2/js/btvn_1.js
@@ -0,0 +1,29 @@
+var city = "Hà Nội";
+let age = 13;
+const PI = 3.14;
+
+var temperature = 28;
+var greeting = "Hello world!";
+var isJavaScriptFun = true;
+var nothing = null;
+var notDefined;
+var uniqueID = Symbol("id");
+var numbers = [1, 2, 3, 4, 5];
+var person = { name: "Châu", age: 13};
+
+var greetingUppercase = greeting.toUpperCase();
+var greetingReplaced = greeting.replace("Hello", "Good afternoon");
+
+console.log(city); 
+console.log(age); 
+console.log(PI); 
+console.log(temperature); 
+console.log(greeting); 
+console.log(isJavaScriptFun); 
+console.log(nothing); 
+console.log(notDefined); 
+console.log(uniqueID); 
+console.log(numbers); 
+console.log(person); 
+console.log(greetingUppercase); 
+console.log(greetingReplaced); 
