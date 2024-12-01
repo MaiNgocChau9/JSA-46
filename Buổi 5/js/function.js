b5fd9850140e357f8791433f1ee74d68d1d98ef6
MaiNgocChau9 <chauaurora9@gmail.com>
2024-12-01 09:33:34 +0700

    Added user authentication and updated database schema in UserController and UserModel.

--- Buổi 5/js/function.js
+++ Buổi 5/js/function.js
@@ -1,15 +1,30 @@
 //! Khai báo function
 //* Cách 1: Hàm có tên
-function tinhTong(a, b) {
-    return a + b
-} 
+// Hàm sum: nhận tham số a, b -> trả về tổng của chúng
+function sum(a, b) {
+    return a + b;
+}
 // Gọi hàm
-console.log(tinhTong(1, 2))
+console.log(sum(1, 3));
 
 //* Cách 2: Hàm không tên/ẩn danh
-const sum = function(a, b) {
-    return a + b
-}
-console.log(sum(1, 2))
+// Hàm sum_2: nhận tham số a, b -> trả về tổng của chúng
+const sum_2 = function(a, b) {
+    return a + b;
+};
+console.log(sum_2(1, 2));
 
-//? Không có RETURN hàm trả về "undifined"
+//! Hàm không có RETURN trả về "undefined"
+// Hàm hello: nhận tham số name -> in ra "Hello " + name
+const hello = function(name) {
+    console.log("Hello " + name);
+};
+console.log(hello("diep"));
+
+//! Lưu trữ key: value vào localStorage
+//* Nếu value không phải chuỗi -> không lưu được
+localStorage.setItem("name", {});
+
+//? Mở DevTools -> Application tab -> Local Storage 
+// -> Tìm domain của trang -> Kiểm tra giá trị lưu
+console.log(localStorage.getItem("name"));
